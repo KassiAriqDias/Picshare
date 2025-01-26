4c018c1b8804a2dec52f75b8150f39e0bbabb682
KassiyetB <kaszi.exe@gmail.com>
2025-01-26 10:56:08 +0500

    create mongodb atlas connection

--- server/server.js
+++ server/server.js
@@ -1,8 +1,19 @@
 const express = require('express');
+const mongoose = require('mongoose');
 const app = express();
+require('dotenv').config();
 const port = 3001;
 
+
 app.use(express.json());
+
+// MongoDB Atlas connection
+const MONGO_PASSWORD = process.env.MONGO_PASSWORD;
+const mongo_url = `mongodb+srv://Kassiyet:${MONGO_PASSWORD}@kassiyet.c2egr.mongodb.net/?retryWrites=true&w=majority&appName=Kassiyet`
+mongoose.connect(mongo_url)
+.then(() => console.log('Connected to MongoDB'))
+.catch((err) => console.log('Error connecting to MongoDB: ', err));
+
 
 
 app.get('/', (req, res) => {
