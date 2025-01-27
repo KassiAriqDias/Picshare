49a23d9c9e6f1769bd78ef52f215c9bd0cd2e6f7
Dias06K <diasfffkami@gmail.com>
2025-01-27 17:18:41 +0500

    implement external apis

--- server/routes/userRoutes.js
+++ server/routes/userRoutes.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const { registerUser, loginUser } = require('../controllers/userController');
+const { registerUser, loginUser, generateName, genearatePFP } = require('../controllers/userController');
 
 // Register route
 router.post('/register', registerUser);
@@ -8,4 +8,10 @@ router.post('/register', registerUser);
 // Login route
 router.post('/login', loginUser);
 
+router.get('/generate_name', generateName);
+
+router.get('/generate_pfp', genearatePFP )
+
 module.exports = router;
+
+
