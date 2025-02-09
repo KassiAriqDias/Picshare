291cb7c9485c21cd8f64b44fa808f56b23920c48
KassiyetB <kaszi.exe@gmail.com>
2025-02-09 20:29:50 +0500

    add News and Quotes

--- frontend/src/components/Announcement/Announcement.js
+++ frontend/src/components/Announcement/Announcement.js
@@ -7,18 +7,13 @@ const Announcement = (promp) => {
     return (
         <div className='announcement'>
             <div className='announcement-header'>
-    
-              <div className='announcement-box-left'>
-                <img className='profile-pic' src={promp.profilePic} alt='profile' />
-                <p className='profile-username'>{promp.username}</p>
-              </div> 
-              
+               <h2>Announcement & Udate</h2>          
             </div>
             <div className='announcement-body' >
-              <img src='https://i.pinimg.com/736x/dc/7d/48/dc7d4866cfcc0cf1f32123d822113230.jpg' alt='post'/>
+              <img src='https://i.pinimg.com/736x/dc/7d/48/dc7d4866cfcc0cf1f32123d822113230.jpg' alt='announcement'/>
               <div className='carusel'>
-                <i class='bx bxs-left-arrow'></i>
-                <i class='bx bxs-right-arrow' ></i>
+                <i className='bx bxs-left-arrow'></i>
+                <i className='bx bxs-right-arrow' ></i>
               </div>
             </div>
             <div className='announcement-footer'>
--- frontend/src/pages/Home/Home.js
+++ frontend/src/pages/Home/Home.js
@@ -3,21 +3,25 @@ import './Home.css'
 import Header from '../../components/Header/Header'
 import Post from '../../components/Post/Post'
 import Announcement from '../../components/Announcement/Announcement'
+//import News from '../../components/News/News'
+import Quotes from '../../components/Quotes/Quotes'
 import Footer from '../../components/Footer/Footer'
 import dummyPost from '../../dummy_data/post.json'
 
 
 const Home = () => {
+
   return (
     <div className='home'>
         <Header/>
         <div className="home-page">
             <Announcement profilePic={'https://i.pinimg.com/736x/d3/ba/35/d3ba351eb31f34000a26a221cf68601f.jpg'} username={"title"} post={'https://i.pinimg.com/736x/dc/7d/48/dc7d4866cfcc0cf1f32123d822113230.jpg'} />
+            {/*<News />*/} 
+            <Quotes/>
             {dummyPost.map( post => {
               return <Post key={post.id} profilePic={post.profile_pic} username={post.username} post={post.post}/>
             })}
         </div>
-        
         <Footer />
     </div>
   )
